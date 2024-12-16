26dac204fc92b2fd987f2e0643c1b9f029c99917
Nmk78 <naymyokhant78@gmail.com>
2024-12-16 13:36:37 +0630

    eslint error

--- app/archive/[archiveId]/[id]/page.tsx
+++ app/archive/[archiveId]/[id]/page.tsx
@@ -1,10 +1,12 @@
 import { ArchiveCandidateDetailPage } from "@/components/ArchiveCandidateDetails";
 
 interface CandidatePageProps {
-  params: { archiveId: string; id: string };
+  params: {
+    archiveId: string;
+    id: string;
+  };
 }
 
 export default function CandidatePage({ params: { archiveId, id } }: CandidatePageProps) {
   return <ArchiveCandidateDetailPage archiveId={archiveId} id={id} />;
 }
-
