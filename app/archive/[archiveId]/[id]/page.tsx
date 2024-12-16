a8877c4b9a1bdbc33256c37b4b56ac923e228b1d
Nmk78 <naymyokhant78@gmail.com>
2024-12-16 14:14:24 +0630

    eslint error 2

--- app/archive/[archiveId]/[id]/page.tsx
+++ app/archive/[archiveId]/[id]/page.tsx
@@ -1,12 +1,10 @@
 import { ArchiveCandidateDetailPage } from "@/components/ArchiveCandidateDetails";
 
-interface CandidatePageProps {
-  params: Promise<{ archiveId: string; id: string }>;
-}
+type Params = Promise<{ archiveId: string; id: string }>;
 
-export default async function CandidatePage({ params }: CandidatePageProps) {
-  const resolvedParams = await params;
+export default async function CandidatePage({ params }: { params: Params }) {
+  const resolvedParams = await params; // Await the promise to get the resolved values
   const { archiveId, id } = resolvedParams;
 
-  return <ArchiveCandidateDetailPage archiveId={archiveId} id={id} />;
+  return <ArchiveCandidateDetailPage id={id} archiveId={archiveId} />;
 }
