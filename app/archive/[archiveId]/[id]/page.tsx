695aeefaf9d0c020b1e3a9dfa28141e1e435792e
Nmk78 <naymyokhant78@gmail.com>
2024-12-16 14:36:32 +0630

    eslint error 6

--- app/archive/[archiveId]/[id]/page.tsx
+++ app/archive/[archiveId]/[id]/page.tsx
@@ -1,3 +1,4 @@
+"use client"
 import { useRouter } from 'next/router';
 import { useEffect, useState } from 'react';
 import { ArchiveCandidateDetailPage } from '@/components/ArchiveCandidateDetails';
