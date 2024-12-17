cffe4385c338ba88a89bfc808190b3a831bff9f3
Nmk78 <naymyokhant78@gmail.com>
2024-12-17 23:09:08 +0630

    pls work pls

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,9 +4,12 @@ import { withUt } from "uploadthing/tw";
 export default withUt({
   darkMode: ["class"],
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./Components/**/*.{js,ts,jsx,tsx,mdx}",
+    // "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    // "./Components/**/*.{js,ts,jsx,tsx,mdx}",
+    // "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
