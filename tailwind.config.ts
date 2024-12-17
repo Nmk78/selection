b2b4b112c0be66986ace5f86592a752c7d8c37df
Nmk78 <naymyokhant78@gmail.com>
2024-12-17 22:31:49 +0630

    i think this should work

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,7 +5,7 @@ export default withUt({
   darkMode: ["class"],
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./Components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
