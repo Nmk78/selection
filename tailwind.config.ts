fb239719262518cbfc031b493514e9bd78273d58
Nmk78 <naymyokhant78@gmail.com>
2024-12-18 00:35:51 +0630

    it work on my maching4

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -91,7 +91,7 @@ import type { Config } from "tailwindcss";
 export default {
   darkMode: ["class"],
   content: [
-    "./Components/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
