7f79856cef815b0306a537262114956088c55a99
Nmk78 <naymyokhant78@gmail.com>
2024-12-18 00:14:00 +0630

    it work on my maching

---
+++ postcss.config.mjs
@@ -0,0 +1,27 @@
+// // /** @type {import('postcss-load-config').Config} */
+// // const config = {
+// //   plugins: {
+// //     tailwindcss: {},
+// //     autoprefixer: {},
+// //   },
+// // };
+
+// // export default config;
+
+
+// // postcss.config.js
+// module.exports = {
+//   plugins: {
+//     tailwindcss: {},
+//     autoprefixer: {},
+//   },
+// }
+
+
+// postcss.config.mjs
+export default {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+};
