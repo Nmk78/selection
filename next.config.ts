1ea73fe3389cde75b514866d428c67e408b967d3
Nmk78 <naymyokhant78@gmail.com>
2024-12-17 22:01:49 +0630

    let's rockgit add .

--- next.config.ts
+++ next.config.ts
@@ -1,43 +1,43 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   images: {
-//     remotePatterns: [
-//       {
-//         protocol: "https",
-//         hostname: "**", // Allow all HTTPS domains
-//       },
-//       {
-//         protocol: "http",
-//         hostname: "**", // Allow all HTTP domains
-//       },
-//     ],
-//     domains: ["localhost", "utfs"], // Add localhost to support local images
-//   },
-  
-// };
-
-// module.exports = nextConfig;
-
-import path from 'path';
-import { NextConfig } from 'next';
-
-const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   images: {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "**",
+        hostname: "**", // Allow all HTTPS domains
       },
       {
         protocol: "http",
-        hostname: "**",
+        hostname: "**", // Allow all HTTP domains
       },
     ],
+    domains: ["localhost", "utfs"], // Add localhost to support local images
   },
-  webpack(config) {
-    config.resolve.alias['@'] = path.resolve(__dirname);
-    return config;
-  },
+  
 };
 
-export default nextConfig;
+module.exports = nextConfig;
+
+// import path from 'path';
+// import { NextConfig } from 'next';
+
+// const nextConfig: NextConfig = {
+//   images: {
+//     remotePatterns: [
+//       {
+//         protocol: "https",
+//         hostname: "**",
+//       },
+//       {
+//         protocol: "http",
+//         hostname: "**",
+//       },
+//     ],
+//   },
+//   webpack(config) {
+//     config.resolve.alias['@'] = path.resolve(__dirname);
+//     return config;
+//   },
+// };
+
+// export default nextConfig;
