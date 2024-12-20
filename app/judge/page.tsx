57e587edcec59e22d96768341511699a24561e6c
Nmk78 <naymyokhant78@gmail.com>
2024-12-20 15:34:34 +0630

    fixing judge voting page

--- app/judge/page.tsx
+++ app/judge/page.tsx
@@ -61,16 +61,18 @@ export default async function JudgeVotingPage() {
       </h1>
       <Suspense
         fallback={
-          <div className="space-y-8">
-            {[...Array(6)].map((_, index) => (
-              <div key={index} className="flex items-center space-x-4">
-                <Skeleton className="h-12 w-12 rounded-full" />
-                <div className="space-y-2">
-                  <Skeleton className="h-4 w-[250px]" />
-                  <Skeleton className="h-4 w-[200px]" />
+          <div className="flex justify-center items-center min-h-screen">
+            <div className="space-y-8">
+              {[...Array(6)].map((_, index) => (
+                <div key={index} className="flex items-center space-x-4">
+                  <Skeleton className="h-12 w-12 rounded-full" />
+                  <div className="space-y-2">
+                    <Skeleton className="h-4 w-[250px]" />
+                    <Skeleton className="h-4 w-[200px]" />
+                  </div>
                 </div>
-              </div>
-            ))}
+              ))}
+            </div>
           </div>
         }
       >
