a4d815dea37acbfb82bf68360d469e24506eb823
Nmk78 <naymyokhant78@gmail.com>
2024-12-27 22:22:10 +0630

    fixed ogs

--- app/archive/[archiveId]/page.tsx
+++ app/archive/[archiveId]/page.tsx
@@ -79,7 +79,7 @@ export default function YearArchivePage() {
       >
         Selected Students
       </motion.h1>
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 sm:gap-8 max-w-7xl mx-auto">
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 sm:gap-8 max-w-7xl mx-auto mb-20">
         {pastCandidates.length > 0 ? (
           pastCandidates.map((candidate, index) => (
             <motion.div
