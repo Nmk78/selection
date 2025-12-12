0a4ba8c2e34f301e017d54f6f4fb8f1e614f4368
Nmk78 <naymyokhant78@gmail.com>
2025-12-12 15:08:00 +0630

    convex to mongodb

--- convex/auth.config.ts
+++ convex/auth.config.ts
@@ -1,3 +1,17 @@
-// When using @convex-dev/auth, the auth.config.ts should be empty.
-// All provider configuration is done in auth.ts via convexAuth().
-export default {};
+// // When using @convex-dev/auth, the auth.config.ts should be empty.
+// // All provider configuration is done in auth.ts via convexAuth().
+// export default {};
+
+export default {
+    providers: [
+        {
+            domain: process.env.CONVEX_SITE_URL || 'http://localhost:3000',
+            applicationID: 'convex',
+        },
+    ],
+    session: {
+        jwt: {
+            durationMs: 60000 * 60 * 24 * 365,
+        },
+    },
+};
